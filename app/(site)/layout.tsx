4a02b942d197234348b1a82f21c87ff1af7c54e3
waqas837 <waqaskhanbughlani1124@gmail.com>
2024-10-16 13:24:52 +0500

    first commit

---
+++ app/(site)/layout.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Lines from "@/components/Lines";
+import ScrollToTop from "@/components/ScrollToTop";
+import { ThemeProvider } from "next-themes";
+import { Inter } from "next/font/google";
+import "../globals.css";
+const inter = Inter({ subsets: ["latin"] });
+
+import ToasterContext from "../context/ToastContext";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import LoadingBar from "react-top-loading-bar";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [progress, setProgress] = useState(0)
+  const pathname = usePathname()
+
+  useEffect(() => {
+    setProgress(20)
+
+    setTimeout(() => {
+      setProgress(40)
+    }, 100);
+
+    setTimeout(() => {
+      setProgress(100)
+    }, 400);
+
+  }, [pathname])
+
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      setProgress(0)
+    }, 50);
+  }, [])
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`dark:bg-black ${inter.className}`}>
+        <LoadingBar
+          color='blue'
+          progress={progress}
+          onLoaderFinished={() => setProgress(0)}
+        />
+        <ThemeProvider
+          enableSystem={false}
+          attribute="class"
+          defaultTheme="light"
+        >
+          <Lines />
+          <Header />
+          <ToasterContext />
+          {children}
+          <Footer />
+          <ScrollToTop />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
