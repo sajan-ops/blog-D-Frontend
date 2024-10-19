58ce1c5657eb7a71d24bcdcfe05fad9f25149e84
waqas837 <waqaskhanbughlani1124@gmail.com>
2024-10-19 09:39:21 +0500

    user forgot passwrod remember me functionalities added in prevoius commit

--- components/Auth/Signup.tsx
+++ components/Auth/Signup.tsx
@@ -171,7 +171,7 @@ const Signup = () => {
                   </button>}
                 </div>
               </div>
-
+ 
               {/* <button
                 aria-label="signup with github"
                 className="text-body-color dark:text-body-color-dark dark:shadow-two mb-6 flex w-full items-center justify-center rounded-sm border border-stroke bg-[#f8f8f8] px-6 py-3 text-base outline-none transition-all duration-300 hover:border-primary hover:bg-primary/5 hover:text-primary dark:border-transparent dark:bg-[#2C303B] dark:hover:border-primary dark:hover:bg-primary/5 dark:hover:text-primary dark:hover:shadow-none"
